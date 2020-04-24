866c2075e074a6958ac9b51333753568e4015b91
Ricardo <63927411+RicardoHernandez07@users.noreply.github.com>
2020-04-23 18:46:05 -0700

    Add files via upload

---
+++ Practica 2/Practica 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lista = new string[10];
+
+
+            for (int i = 0; i < lista.Length; i++)  
+            {
+                Console.Write($"Ingrese el nombre de 10 personas { (i + 1) }:");
+                lista[i] = Console.ReadLine();
+
+            } 
+            for (int i = 0 ; i < lista.Length;i++)
+            {
+                Console.WriteLine($"El nombre de Persona {(i + 1)} es: {lista[i]}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
